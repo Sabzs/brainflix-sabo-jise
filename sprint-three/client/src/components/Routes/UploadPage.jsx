b989673f65f656480f6d494921cac86c1ebd402d
Sabzs <sajiyoonline@gmail.com>
2020-08-23 21:13:58 -0700

    modified header style in both header and upload components

--- sprint-three/client/src/components/Routes/UploadPage.jsx
+++ sprint-three/client/src/components/Routes/UploadPage.jsx
@@ -8,31 +8,33 @@ class UploadPage extends React.Component {
   render() {
     return (
       <div className="upload">
-        <header className="upload__headerDiv">
-          <div className="upload__logoDiv">
-            <Link to="/">
-              <img className="upload__logo" src={Logo} alt="brainflix logo" />
+        <header className="header">
+          <div className="header__logoDiv">
+            <Link to="/Home">
+              <img className="header__logo" src={Logo} alt="brainflix logo" />
             </Link>
           </div>
 
-          <div className="upload__searchUpload">
-            <div className="upload__searchDiv">
+          <div className="header__searchUpload">
+            <div className="header__searchDiv">
               <input
-                className="upload__searchInput"
+                className="header__searchInput"
                 type="search"
                 placeholder="Search"
               />
             </div>
-            <div className="upload__upload-mohead">
-              <div className="upload__btnDiv">
-                <button className="upload__btn" type="upload">
-                  <p>UPLOAD </p>
-                </button>
+            <div className="header__upload-mohead">
+              <div className="header__btnDiv">
+                <Link to="/uploadpage">
+                  <button className="header__btn" type="upload">
+                    UPLOAD
+                  </button>
+                </Link>
               </div>
 
-              <div className="upload__moheadDiv">
+              <div className="header__moheadDiv">
                 <img
-                  className="upload__mohead-img"
+                  className="header__mohead-img"
                   src={Images}
                   alt="Muhan Muruge Icon"
                 />
