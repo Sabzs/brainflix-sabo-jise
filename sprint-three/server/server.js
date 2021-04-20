48bedc57f65f5e25f8b0ccc376bb9800751dab9e
Sabzs <sajiyoonline@gmail.com>
2021-04-20 10:31:43 -0700

    renamed app.js to server.js

--- sprint-three/server/server.js
+++ sprint-three/server/server.js
@@ -3,12 +3,14 @@ const app = express();
 const cors = require("cors")
 require("dotenv").config();
 const { PORT, BACKEND_URL } = process.env;
-const videoRoutes = require("./routes/videos");
 
+
+const videoRoutes = require("./routes/videos");
 
 
 app.use(express.json());
 app.use(cors());
+
 app.use("/videos", videoRoutes);
 
 app.listen(PORT, () => console.log(`listening at: ${BACKEND_URL}:${PORT}`));
