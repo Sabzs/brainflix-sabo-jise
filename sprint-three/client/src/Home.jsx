b87d824bb50e824e5e1e0a2dd1cb80b3394cd55e
Sabzs <sajiyoonline@gmail.com>
2021-04-21 12:48:02 -0700

    refactored API with Async/await, getting default HeroVideo ID data

--- sprint-three/client/src/Home.jsx
+++ sprint-three/client/src/Home.jsx
@@ -10,46 +10,36 @@ import { API_KEY } from "./env/env_variables";
 
 class Home extends Component {
   state = {
-    sidebarVideos: [],
+    sideVideos: [],
     mainVideo: {},
     videoId: "",
   };
 
   componentDidMount() {
     axios
-      .get(`https://project-2-api.herokuapp.com/videos/?api_key=${API_KEY}`)
-      .then((response) => {
-        console.log(response.data);
+      .get("http://localhost:8080/videos/sidevideos")
+      .then(async (response) => {
         this.setState({
-          sidebarVideos: response.data,
-          videoId: response.data.id,
+          sideVideos: response.data,
         });
-
-        axios
-          .get(
-            `https://project-2-api.herokuapp.com/videos/1af0jruup5gu/?api_key=${API_KEY}`
-          )
-          .then((response) => {
-            console.log(response.data);
-            this.setState({ mainVideo: response.data });
-          });
+        const homeObject = await axios.get(
+          // "http://localhost:8080/videos/sidevideos/1af0jruup5gu"
+          `https://project-2-api.herokuapp.com/videos/1af0jruup5gu/?api_key=${API_KEY}`
+        );
+        console.log(homeObject.data);
+        this.setState({ mainVideo: homeObject.data });
       });
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // console.log(prevState);
-    // console.log(this.props);
     if (
       this.props.match.params.id !== undefined &&
       this.props.match.params.id !== prevState.mainVideo.id
     ) {
       let id = this.props.match.params.id;
       axios
-        .get(
-          `https://project-2-api.herokuapp.com/videos/${id}?api_key=${API_KEY}`
-        )
+        .get(`http://localhost:8080/videos/mainvideo/${id}`)
         .then((response) => {
-          // console.log(response.data);
           this.setState({
             mainVideo: response.data,
             videoId: response.data.id,
@@ -69,15 +59,10 @@ class Home extends Component {
               <HeroVideoInfo mainContent={this.state.mainVideo} />
               <FormInputs comments={this.state.mainVideo} />
             </div>
-            <div className="videoItems">
-              <div className="video-list__headerDiv">
-                <h4 className="video-list__header">Next Video</h4>
-              </div>
-              <VideoList
-                nextVideo={this.state.sidebarVideos}
-                mainVideo={this.state.mainVideo}
-              />
-            </div>
+            <VideoList
+              nextVideo={this.state.sideVideos}
+              mainVideo={this.state.mainVideo}
+            />
           </div>
         </div>
       </div>
