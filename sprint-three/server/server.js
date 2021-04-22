f29ac19796dddea263be163cfece7b3b9daeaee6
Sabzs <sajiyoonline@gmail.com>
2021-04-22 12:31:09 -0700

    modified for Heroku deployment

--- sprint-three/server/server.js
+++ sprint-three/server/server.js
@@ -1,16 +1,31 @@
 const express = require("express");
-const app = express();
+const path = require("path");
 const cors = require("cors")
+const app = express();
 require("dotenv").config();
-const { PORT, BACKEND_URL } = process.env;
+const PORT = process.env.PORT || 8080;
 
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const videoRoutes = require("./routes/videos");
-
-
-app.use(express.json());
-app.use(cors());
-
 app.use("/videos", videoRoutes);
 
-app.listen(PORT, () => console.log(`listening at: ${BACKEND_URL}:${PORT}`));
+
+//heroku deplyment==//
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('../client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+  });
+}
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`)
+});
