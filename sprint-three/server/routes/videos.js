b2a62ada44ab129018fb3303eed46864bdce6bb9
Sabzs <sajiyoonline@gmail.com>
2021-05-03 09:42:00 -0700

    no changes

--- sprint-three/server/routes/videos.js
+++ sprint-three/server/routes/videos.js
@@ -45,7 +45,7 @@ router.get("/mainvideo/:id", (req, res) => {
   });
 });
 
-//post 
+//post video==//
 router.post("/", (req, res) => {
   console.log("post route")
   const newVideo = {
@@ -57,29 +57,7 @@ router.post("/", (req, res) => {
     views: 55066,
     likes: 60775,
     timestamp: helper.timestamp(),
-    comments: [
-      {
-        "name": "Micheal Lyons",
-        "comment": "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of acconcert I have EVER witnessed.",
-        "id": "1ab6d9f6-da38-456e-9b09-ab0acd9ce818",
-        "likes": 0,
-        "timestamp": 1545162149101
-      },
-      {
-        "name": "Gary Wong",
-        "comment": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
-        "id": "cc6f173d-9e9d-4501-918d-bc11f15a8e14",
-        "likes": 0,
-        "timestamp": 1544595784057
-      },
-      {
-        "name": "Theodore Duncan",
-        "comment": "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
-        "id": "993f950f-df99-48e7-bd1e-d95003cc98f1",
-        "likes": 0,
-        "timestamp": 1542262986076
-      }
-    ]
+    comments: []
   }
   if (!newVideo.title || !newVideo.description) {
     return res.status(400).json({
@@ -91,5 +69,6 @@ router.post("/", (req, res) => {
   helper.writeJSONFile(videosData, videos)
   res.json(newVideo.id);
 });
+
 
 module.exports = router;
